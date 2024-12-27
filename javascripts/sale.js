432153058228a124359e30f7fb032c9fa5f8637e
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2024-12-27 12:07:26 +0900

    Add addProduct()

--- javascripts/sale.js
+++ javascripts/sale.js
@@ -1,5 +1,27 @@
+const form = document.querySelector("form");
+const products = [];
+
+function addProduct() {
+  products.push({
+    price: parseInt(form.product.value),
+    quantity: parseInt(form.quantity.value),
+  });
+  console.info("Current products", products);
+  form.reset();
+}
+
 function calc() {
-  const form = document.querySelector("form");
-  const total = parseInt(form.product.value) * parseInt(form.quantity.value);
-  console.info(`合計金額： ${total}円`);
+  const message = products.reduce((result, currentProduct) => {
+    const messageLine = `${currentProduct.price}円： ${currentProduct.quantity}点\n`;
+    result += messageLine;
+    return result;
+  }, "");
+
+  const total = products.reduce((result, currentProduct) => {
+    const subtotal = currentProduct.price * currentProduct.quantity;
+    result += subtotal;
+    return result;
+  }, 0);
+
+  window.alert(message + "\n" + `合計金額： ${total}円`);
 }
