2f8f7bf7c6e6b72825678b1ae9eafb256cdf4eda
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2024-12-27 12:47:34 +0900

    Show subtotal when the Add button is clicked

--- javascripts/sale.js
+++ javascripts/sale.js
@@ -1,48 +1,57 @@
 const form = document.querySelector("form");
 const purchases = [];
 
-function addProduct() {
+function addToCart() {
   purchases.push({
     price: parseInt(form.product.value),
     quantity: parseInt(form.quantity.value),
   });
-  console.info("Current products", purchases);
+  window.alert(itemsInCartMessage());
   form.reset();
 }
 
-function calc() {
-  const message = purchases.reduce((result, currentProduct) => {
-    const messageLine = `${currentProduct.price}円: ${currentProduct.quantity}点\n`;
-    result += messageLine;
-    return result;
-  }, "");
-
-  const itemTotal = purchases.reduce((result, currentProduct) => {
+function itemTotal() {
+  return purchases.reduce((result, currentProduct) => {
     const subtotal = currentProduct.price * currentProduct.quantity;
     result += subtotal;
     return result;
   }, 0);
+}
 
-  const shippingFee = _shippingFee(itemTotal);
-  const orderTotal = itemTotal + shippingFee;
+function itemLines() {
+  return purchases.reduce((result, currentProduct) => {
+    const messageLine = `${currentProduct.price}円: ${currentProduct.quantity}点\n`;
+    result += messageLine;
+    return result;
+  }, "");
+}
+
+function shippingFee(itemTotal) {
+  if (itemTotal === 0) {
+    return 0;
+  } else if (itemTotal < 2000) {
+    return 500;
+  } else if (2000 <= itemTotal && itemTotal < 3000) {
+    return 250;
+  } else if (itemTotal >= 3000) {
+    return 0;
+  }
+}
+
+function itemsInCartMessage() {
+  return itemLines() + "\n" + `小計: ${itemTotal()}円`;
+}
+
+function calc() {
+  const _itemLines = itemLines();
+  const _itemTotal = itemTotal();
+  const _shippingFee = shippingFee(_itemTotal);
 
   window.alert(
-    message +
+    _itemLines +
       "\n" +
-      `送料: ${shippingFee}円` +
+      `送料: ${_shippingFee}円` +
       "\n" +
-      `合計金額: ${orderTotal}円`
+      `合計金額: ${_itemTotal + _shippingFee}円`
   );
-
-  function _shippingFee(itemTotal) {
-    if (itemTotal === 0) {
-      return 0;
-    } else if (itemTotal < 2000) {
-      return 500;
-    } else if (2000 <= itemTotal < 3000) {
-      return 250;
-    } else if (itemTotal >= 3000) {
-      return 0;
-    }
-  }
 }
