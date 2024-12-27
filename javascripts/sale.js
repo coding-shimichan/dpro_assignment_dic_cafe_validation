e9b1f6610dd7ea75f12af11c9f50b4c7c209dd5a
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2024-12-27 12:25:33 +0900

    Calculate shippingFee

--- javascripts/sale.js
+++ javascripts/sale.js
@@ -12,16 +12,37 @@ function addProduct() {
 
 function calc() {
   const message = purchases.reduce((result, currentProduct) => {
-    const messageLine = `${currentProduct.price}円： ${currentProduct.quantity}点\n`;
+    const messageLine = `${currentProduct.price}円: ${currentProduct.quantity}点\n`;
     result += messageLine;
     return result;
   }, "");
 
-  const total = purchases.reduce((result, currentProduct) => {
+  const itemTotal = purchases.reduce((result, currentProduct) => {
     const subtotal = currentProduct.price * currentProduct.quantity;
     result += subtotal;
     return result;
   }, 0);
 
-  window.alert(message + "\n" + `合計金額： ${total}円`);
+  const shippingFee = _shippingFee(itemTotal);
+  const orderTotal = itemTotal + shippingFee;
+
+  window.alert(
+    message +
+      "\n" +
+      `送料: ${shippingFee}円` +
+      "\n" +
+      `合計金額: ${orderTotal}円`
+  );
+
+  function _shippingFee(itemTotal) {
+    if (itemTotal === 0) {
+      return 0;
+    } else if (itemTotal < 2000) {
+      return 500;
+    } else if (2000 <= itemTotal < 3000) {
+      return 250;
+    } else if (itemTotal >= 3000) {
+      return 0;
+    }
+  }
 }
